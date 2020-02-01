fc3e2c815eb24034a7aff29c0d2e850ee1be3725
Nick <pattison.nick@gmail.com>
2020-02-01 02:19:54 -0500

    Built a card GUI system
    Press SPACE to switch between inspect / mini mode.

---
+++ Assets/Scripts/GUI/CardGUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardGUI : MonoBehaviour {
+
+    Quaternion targetRotation;
+    Vector2 targetPosition;
+    float targetScale = 1;
+    float delay = 0;
+
+    RectTransform rt;
+    void Start() {
+        rt = transform as RectTransform;
+    }
+    public void SetDepth(int i) {
+        GetComponent<Canvas>().sortingOrder = i;
+    }
+    public void Tint() {
+        GetComponent<Image>().color = Color.red;
+    }
+    void Update() {
+        if (delay > 0) {
+            delay -= Time.deltaTime;
+            return;
+        }
+        rt.localRotation = MathStuff.Damp(transform.localRotation, targetRotation, .01f);
+        rt.anchoredPosition = MathStuff.Damp(rt.anchoredPosition, targetPosition, .001f);
+        transform.localScale = Vector3.one * MathStuff.Damp(transform.localScale.x, targetScale, .01f);
+    }
+    public void AnimateTo(Vector3 pos, Quaternion rot, float scale, float delay) {
+        targetPosition = pos;
+        targetRotation = rot;
+        targetScale = scale;
+        this.delay = delay;
+    }
+}
---
+++ Assets/Scripts/GUI/CardManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardManager : MonoBehaviour
+{
+
+    public enum State {
+        Mini,
+        Inspect
+    }
+
+    public CardGUI cardGUIPrefab;
+    public RectTransform areaLoadout;
+
+    State state = State.Mini;
+    List<CardGUI> cards = new List<CardGUI>(); // card index 0 should be on bottom
+
+    void Start() {
+        AddCard(new Card());
+        AddCard(new Card());
+        AddCard(new Card());
+        AddCard(new Card());
+        AddCard(new Card());
+    }
+    
+    public void AddCards(Card[] cards) {
+        foreach(Card card in cards) {
+            AddCard(card);
+        }
+    }
+    public void AddCard(Card card) {
+        CardGUI newCard = Instantiate(cardGUIPrefab, transform);
+        // TODO: copy stats and stuff over to card GUI
+        if (cards.Count == 0) newCard.Tint();
+        cards.Add(newCard);
+        PositionCards();
+    }
+    void PositionCards() {
+
+        for(int i = 0; i < cards.Count; i++) {
+            cards[i].SetDepth(cards.Count - i);
+        }
+
+        switch (state) {
+            case State.Mini:
+                PositionCardsMini();
+                break;
+            case State.Inspect:
+                PositionCardsInspect();
+                break;
+        }
+    }
+    void PositionCardsMini() {
+        float dy = 50;
+        for (int i = 0; i < cards.Count; i++) {
+            Vector3 pos = new Vector2(0, 0 + dy * i);
+            Quaternion rot = Quaternion.Euler(0, 180, 90);
+            float scale = 1;
+            cards[i].AnimateTo(pos, rot, scale, (cards.Count - i) * .1f);
+            cards[i].transform.SetParent(areaLoadout);
+        }
+    }
+    void PositionCardsInspect() {
+        float dx = 150;
+        float x = -dx * (cards.Count - 1) / 2f;
+        for (int i = 0; i < cards.Count; i++) {
+            Vector3 pos = new Vector2(x + dx * i, 0);
+            Quaternion rot = Quaternion.identity;
+            float scale = 2;
+            cards[i].AnimateTo(pos, rot, scale, i * .1f);
+            cards[i].transform.SetParent(transform);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            state = (state == State.Inspect) ? State.Mini : State.Inspect;
+            PositionCards();
+        }
+    }
+}
