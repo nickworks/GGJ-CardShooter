69139ef8b404d62c3fa03aa74c16e212ceea8c83
Nick <pattison.nick@gmail.com>
2020-02-02 12:22:54 -0500

    Added card pickup object
    The player can collide with these objects to pickup a new card. The card is added to their current tome.

---
+++ Assets/Scripts/Cards and Tomes/PickupCard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupCard : MonoBehaviour
+{
+    Card card;
+
+    void Start() {
+        card = Card.Random();
+    }
+
+    void OnTriggerEnter(Collider collider) {
+        PlayerController pc = collider.GetComponent<PlayerController>();
+        if (pc) {
+            pc.GetComponent<Pawn>().PickupCard(card);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/JustRotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JustRotate : MonoBehaviour
+{
+    public float speed = 90;
+    public bool alsoBounce = true;
+    void Update()
+    {
+        float degrees = speed * Time.deltaTime;
+
+        transform.Rotate(0, degrees, 0);
+        if (alsoBounce) {
+            float y = Mathf.Sin(Time.time * 5) * .25f;
+            transform.localPosition = new Vector3(0, y, 0);
+        }
+    }
+}
