c15361055fc21dddd5f0e7607741771f1121deb4
Nick <pattison.nick@gmail.com>
2020-02-02 09:01:20 -0500

    Organized scripts
    + Scripts/Cards and Tomes/
    + Scripts/Dungeon Gen/

---
+++ Assets/Scripts/Dungeon Gen/DoorSensor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSensor : MonoBehaviour
+{
+    bool needDoorSet = true;
+    public GameObject door;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(needDoorSet && door != null)
+        {
+            door.transform.localPosition = new Vector3(door.transform.localPosition.x, 6, door.transform.localPosition.y);
+            needDoorSet = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Dungeon Gen/Generate_Level.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generate_Level : MonoBehaviour
+{
+    public List<GameObject> levels;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject level = Instantiate(levels[(int) Random.Range(0, levels.Count)]);
+
+        level.transform.position = Vector3.zero;
+    }
+    
+}
